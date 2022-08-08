26afdf0d0173eef8011289f8fb80622dc8bf02a6
Miltownee <72917466+Miltownee@users.noreply.github.com>
2022-08-08 22:04:39 +0200

    feat: added static Main method

--- Diablo/Program.cs
+++ Diablo/Program.cs
@@ -1,1 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
+//[STAThread] 
+namespace Diablo{
+using Diablo.HeroClasses;
+using Diablo.HeroClasses.SubClassesToHeroes;
+using Diablo.Items;
+using Diablo.Items.Armour;
+using Diablo.Items.Weapon;
+    public class Program
+    {
+        // Uncomment the following line to resolve.
+        static void Main(string[] args) {
+            Console.WriteLine("Hello DiabloFans!");
+        }
+    }
+}
